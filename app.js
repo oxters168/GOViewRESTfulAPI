ecdf8649d3e6f9a16b10b255a136de8707a7e4dd
Oxters Wyzgowski <oxters168@gmail.com>
2020-05-05 23:45:40 +0300

    Added steam-user and @yaroslav-95's globaloffensive node packages. Wrote basic log in and open csgo code.

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const app = express();
+const SteamUser = require('steam-user');
+const GlobalOffensive = require('@yaroslav-95/globaloffensive');
 
+let user = new SteamUser();
+let csgo = new GlobalOffensive(user);
+//Log in our steam user
+user.logOn({
+    accountName: "username",
+    password: "password"
+});
+//When steam user logs in, start CSGO
+user.on('loggedOn', () => {
+    console.log("Logged in to Steam");
+    user.gamesPlayed([730]);
+});
+csgo.on("connectedToGC", () => {
+    console.log("Connected to GC!");
+});
+
+//POST request thing
 app.use((req, res, next) => {
     res.status(200).json({
         message: 'It Works'
