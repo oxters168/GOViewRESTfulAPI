36b3358d98d1e532b93ff26c1f03ffad178bd2da
Oxters Wyzgowski <oxters168@gmail.com>
2020-05-06 02:22:31 +0300

    Created GOView route to filter requests for GOView specifically. Moved steam stuff to GOView.js. Added morgan package that logs stuff. Added nodemon package that auto restarts server on changes. Changed the way errors are handled. Added CORS error avoidance. The GOView route now takes matchId, outcomeId, and token as params and returns them in the response for now.

---
+++ api/routes/GOView.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+
+const SteamUser = require('steam-user');
+const GlobalOffensive = require('@yaroslav-95/globaloffensive');
+
+let user = new SteamUser();
+let csgo = new GlobalOffensive(user);
+//Log in our steam user
+user.logOn({
+    accountName: "username",
+    password: "password"
+});
+//When steam user logs in, start CSGO
+user.on('loggedOn', () => {
+    console.log("Logged in to Steam");
+    user.gamesPlayed([730]);
+});
+csgo.on("connectedToGC", () => {
+    console.log("Connected to GC!");
+});
+
+router.get('/', (req, res, next) => {
+    const match = {
+        matchId: req.query['matchId'],
+        outcomeId: req.query['outcomeId'],
+        token: req.query['token']
+    }
+    res.status(200).json({
+        message: 'Requested match',
+        match: match
+    });
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,28 +1,45 @@
 const express = require('express');
 const app = express();
-const SteamUser = require('steam-user');
-const GlobalOffensive = require('@yaroslav-95/globaloffensive');
+const debugLog = require('morgan');
+const bodyParser = require('body-parser');
 
-let user = new SteamUser();
-let csgo = new GlobalOffensive(user);
-//Log in our steam user
-user.logOn({
-    accountName: "username",
-    password: "password"
-});
-//When steam user logs in, start CSGO
-user.on('loggedOn', () => {
-    console.log("Logged in to Steam");
-    user.gamesPlayed([730]);
-});
-csgo.on("connectedToGC", () => {
-    console.log("Connected to GC!");
-});
+const GOViewRoutes = require('./api/routes/GOView');
 
-//POST request thing
+//Logs requests as they come in
+app.use(debugLog('dev'));
+//Parses bodies with json (not actually using this currently)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Avoid CORS error
 app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'It Works'
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+//Filters all requests received with GOView in the url and funnels them to GOView.js
+app.use('/GOView', GOViewRoutes);
+
+//If message was not handled by any routes, pass not found error
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+//Handle all errors
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
     });
 });
 
