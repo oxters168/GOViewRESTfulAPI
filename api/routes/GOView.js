64f07a527a3f6ecf143527e3034a2f9c6daed827
Oxters Wyzgowski <oxters168@gmail.com>
2020-05-06 18:32:49 +0300

    Made RequestGame check if csgo is running or else give error. Changed how logging in works to be through a function, that way it can be called when needed. Added dotenv package to allow for environment variables. Changed port number to be more unique.

--- api/routes/GOView.js
+++ api/routes/GOView.js
@@ -1,24 +1,15 @@
 const express = require('express');
 const router = express.Router();
+require('dotenv').config();
 
 const SteamUser = require('steam-user');
 const GlobalOffensive = require('@yaroslav-95/globaloffensive');
 
 let user = new SteamUser();
 let csgo = new GlobalOffensive(user);
-//Log in our steam user
-user.logOn({
-    accountName: "username",
-    password: "password"
-});
-//When steam user logs in, start CSGO
-user.on('loggedOn', () => {
-    console.log("Logged in to Steam");
-    user.gamesPlayed([730]);
-});
-csgo.on("connectedToGC", () => {
-    console.log("Connected to GC!");
-});
+
+//Log in our steam user when server starts
+SteamLoginWithEnv();
 
 router.get('/RequestGame', (req, res, next) => {
     const match = {
@@ -26,10 +17,47 @@ router.get('/RequestGame', (req, res, next) => {
         outcomeId: req.query['outcomeId'],
         token: req.query['token']
     }
-    res.status(200).json({
-        message: 'Requested match',
-        match: match
-    });
+    
+    if (csgo._isInCSGO) {
+        res.status(200).json({
+            message: 'Requested match',
+            match: match
+        });
+    }
+    else {
+        SteamLoginWithEnv();
+
+        const error = new Error("Could not send request at this time, please try again later. If issue persists, contact support.");
+        error.status = 500;
+        next(error);
+    }
+});
+
+function SteamLogin(username, password) {
+    if (!(username === "" || password === "")) {
+        user.logOn({
+            accountName: username,
+            password: password
+        });
+    }
+    else {
+        console.log("Skipping log in");
+    }
+}
+function SteamLoginWithEnv() {
+    SteamLogin(process.env.USERNAME || "", process.env.PASSWORD || "");
+}
+
+//When steam user logs in, start CSGO
+user.on('loggedOn', () => {
+    console.log("Logged in to Steam");
+    user.gamesPlayed([730]);
+});
+csgo.on("connectedToGC", () => {
+    console.log("Connected to GC");
+});
+user.on('error', (err) => {
+    console.log(err);
 });
 
 module.exports = router;
